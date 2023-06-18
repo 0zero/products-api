0fc52979073b7102d5b82de7e0b191990d9b6f35
Japheth Yates <japheth.yates@gmail.com>
2023-06-18 19:47:24 +0100

    products table integrations tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,11 @@ from typing import Generator
 
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
 
 from src.config import Settings, get_settings
-from src.database.session import Session, SessionLocal
+from src.database.models.base import Base
 from src.main import get_app
 
 
@@ -32,4 +34,16 @@ def test_db() -> Generator[Session, Session, None]:
     """
     Generator for a test DB to be used in tests.
     """
-    yield SessionLocal()
+    test_settings = get_settings_override()
+    test_engine = create_engine(test_settings.database_url)
+    test_session_local = sessionmaker(
+        autocommit=False, autoflush=False, bind=test_engine
+    )
+    connection = test_engine.connect()
+    transaction = connection.begin()
+    test_session = test_session_local(bind=connection)
+    Base.metadata.create_all(bind=test_engine)
+    yield test_session
+    test_session.close()
+    transaction.rollback()
+    connection.close()
--- tests/test_crud.py
+++ tests/test_crud.py
@@ -23,3 +23,73 @@ def test_create_product(test_db: Session) -> None:
     assert product_creacted.Category == "test category"
     assert product_creacted.Variety == "test variety"
     assert product_creacted.Packaging == "test packaging"
+
+
+def test_get_product(test_db: Session) -> None:
+    product = ProductCreate(
+        Category="test category", Variety="test variety", Packaging="test packaging"
+    )
+    product_crud = CRUDProduct(Product)
+    product_creacted = product_crud.create(db=test_db, obj_in=product)
+    product_get = product_crud.get(db=test_db, id=product_creacted.id)
+    assert product_get.id is not None
+    assert product_get.Category == "test category"
+    assert product_get.Variety == "test variety"
+    assert product_get.Packaging == "test packaging"
+
+
+def test_get_many_products(test_db: Session) -> None:
+    product1 = ProductCreate(
+        Category="test category1", Variety="test variety1", Packaging="test packaging1"
+    )
+    product2 = ProductCreate(
+        Category="test category2", Variety="test variety2", Packaging="test packaging2"
+    )
+    product_crud = CRUDProduct(Product)
+    product_creacted1 = product_crud.create(db=test_db, obj_in=product1)
+    product_creacted2 = product_crud.create(db=test_db, obj_in=product2)
+    product_get_many = product_crud.get_multi(db=test_db)
+    assert len(product_get_many) > 1
+    assert product_get_many[0].id is not None
+    assert product_get_many[0].Category == product_creacted1.Category
+    assert product_get_many[0].Variety == product_creacted1.Variety
+    assert product_get_many[0].Packaging == product_creacted1.Packaging
+    assert product_get_many[1].id is not None
+    assert product_get_many[1].Category == product_creacted2.Category
+    assert product_get_many[1].Variety == product_creacted2.Variety
+    assert product_get_many[1].Packaging == product_creacted2.Packaging
+
+
+def test_update_product(test_db: Session) -> None:
+    product = ProductCreate(
+        Category="test category", Variety="test variety", Packaging="test packaging"
+    )
+    product_crud = CRUDProduct(Product)
+    product_creacted = product_crud.create(db=test_db, obj_in=product)
+    product_update = ProductCreate(
+        Category="test category 2",
+        Variety="test variety 2",
+        Packaging="test packaging 2",
+    )
+    product_updated = product_crud.update(
+        db=test_db, db_obj=product_creacted, obj_in=product_update
+    )
+    assert product_updated.id is not None
+    assert product_updated.Category == "test category 2"
+    assert product_updated.Variety == "test variety 2"
+    assert product_updated.Packaging == "test packaging 2"
+
+
+def test_delete_product(test_db: Session) -> None:
+    product = ProductCreate(
+        Category="test category", Variety="test variety", Packaging="test packaging"
+    )
+    product_crud = CRUDProduct(Product)
+    product_creacted = product_crud.create(db=test_db, obj_in=product)
+    product_deleted = product_crud.remove(db=test_db, id=product_creacted.id)
+    product_get = product_crud.get(db=test_db, id=product_creacted.id)
+    assert product_deleted.id is not None
+    assert product_deleted.Category == "test category"
+    assert product_deleted.Variety == "test variety"
+    assert product_deleted.Packaging == "test packaging"
+    assert product_get is None
