c8f6406c2b2f9561f9d11054a01ad04e3387b2bb
Japheth Yates <japheth.yates@gmail.com>
2023-06-18 12:33:23 +0100

    add test DB generator to conftests and start testing CRUD.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,11 @@ import os
 from typing import Generator
 
 import pytest
-from starlette.testclient import TestClient
+from fastapi.testclient import TestClient
 
 from src.config import Settings, get_settings
 from src.main import get_app
+from src.database.session import SessionLocal, Session
 
 
 def get_settings_override():
@@ -24,3 +25,11 @@ def test_app() -> Generator[TestClient, TestClient, None]:
     app.dependency_overrides[get_settings] = get_settings_override
     with TestClient(app) as test_client:
         yield test_client
+
+
+@pytest.fixture(scope="session")
+def test_db() -> Generator[Session, Session, None]:
+    """
+    Generator for a test DB to be used in tests.
+    """
+    yield SessionLocal()
---
+++ tests/test_crud.py
@@ -0,0 +1,30 @@
+import pytest
+
+from src.database.crud.product import CRUDProduct
+from src.database.schemas.product import ProductCreate
+from src.database.models.product import Product
+
+from src.database.crud.order import CRUDOrder
+from src.database.schemas.order import OrderCreate, ProductOrderType
+from src.database.models.order import ProductType, Order
+
+
+from src.database.crud.organisation import CRUDOrganisation
+from src.database.schemas.organisation import OrganisationCreate
+from src.database.models.organisation import Organisation
+
+from src.database.models.base import OrderTypeEnum, OrganisationTypeEnum
+
+from sqlalchemy.orm import Session
+
+
+def test_create_product(test_db: Session) -> None:
+    product = ProductCreate(
+        Category="test category", Variety="test variety", Packaging="test packaging"
+    )
+    product_crud = CRUDProduct(Product)
+    product_creacted = product_crud.create(db=test_db, obj_in=product)
+    assert product_creacted.id is not None
+    assert product_creacted.Category == "test category"
+    assert product_creacted.Variety == "test variety"
+    assert product_creacted.Packaging == "test packaging"
