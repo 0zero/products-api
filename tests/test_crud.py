cab32a4892c0a850c9894e51e88e28cfc24bbb4d
Japheth Yates <japheth.yates@gmail.com>
2023-06-18 20:33:23 +0100

    refactor tests to include fixtures

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -13,83 +13,131 @@ from src.database.schemas.organisation import OrganisationCreate
 from src.database.schemas.product import ProductCreate
 
 
-def test_create_product(test_db: Session) -> None:
-    product = ProductCreate(
-        Category="test category", Variety="test variety", Packaging="test packaging"
+@pytest.fixture()
+def test_product_one() -> ProductCreate:
+    return ProductCreate(
+        Category="test category 1",
+        Variety="test variety 1",
+        Packaging="test packaging 1",
     )
-    product_crud = CRUDProduct(Product)
-    product_creacted = product_crud.create(db=test_db, obj_in=product)
-    assert product_creacted.id is not None
-    assert product_creacted.Category == "test category"
-    assert product_creacted.Variety == "test variety"
-    assert product_creacted.Packaging == "test packaging"
 
 
-def test_get_product(test_db: Session) -> None:
-    product = ProductCreate(
-        Category="test category", Variety="test variety", Packaging="test packaging"
-    )
-    product_crud = CRUDProduct(Product)
-    product_creacted = product_crud.create(db=test_db, obj_in=product)
-    product_get = product_crud.get(db=test_db, id=product_creacted.id)
-    assert product_get.id is not None
-    assert product_get.Category == "test category"
-    assert product_get.Variety == "test variety"
-    assert product_get.Packaging == "test packaging"
-
-
-def test_get_many_products(test_db: Session) -> None:
-    product1 = ProductCreate(
-        Category="test category1", Variety="test variety1", Packaging="test packaging1"
-    )
-    product2 = ProductCreate(
-        Category="test category2", Variety="test variety2", Packaging="test packaging2"
-    )
-    product_crud = CRUDProduct(Product)
-    product_creacted1 = product_crud.create(db=test_db, obj_in=product1)
-    product_creacted2 = product_crud.create(db=test_db, obj_in=product2)
-    product_get_many = product_crud.get_multi(db=test_db)
-    assert len(product_get_many) > 1
-    assert product_get_many[-2].id is not None
-    assert product_get_many[-2].Category == product_creacted1.Category
-    assert product_get_many[-2].Variety == product_creacted1.Variety
-    assert product_get_many[-2].Packaging == product_creacted1.Packaging
-    assert product_get_many[-1].id is not None
-    assert product_get_many[-1].Category == product_creacted2.Category
-    assert product_get_many[-1].Variety == product_creacted2.Variety
-    assert product_get_many[-1].Packaging == product_creacted2.Packaging
-
-
-def test_update_product(test_db: Session) -> None:
-    product = ProductCreate(
-        Category="test category", Variety="test variety", Packaging="test packaging"
-    )
-    product_crud = CRUDProduct(Product)
-    product_creacted = product_crud.create(db=test_db, obj_in=product)
-    product_update = ProductCreate(
+@pytest.fixture()
+def test_product_two() -> ProductCreate:
+    return ProductCreate(
         Category="test category 2",
         Variety="test variety 2",
         Packaging="test packaging 2",
     )
+
+
+@pytest.fixture()
+def test_product_three() -> ProductCreate:
+    return ProductCreate(
+        Category="test category 3",
+        Variety="test variety 3",
+        Packaging="test packaging 3",
+    )
+
+
+@pytest.fixture()
+def test_order_one_no_references(test_product_one: ProductCreate) -> OrderCreate:
+    return OrderCreate(
+        Type=OrderTypeEnum.SELL,
+        Products=[test_product_one],
+        Organisation_id=1,
+    )
+
+
+@pytest.fixture()
+def test_order_two_with_references(
+    test_product_one: ProductCreate, test_product_two: ProductCreate
+) -> OrderCreate:
+    return OrderCreate(
+        Type=OrderTypeEnum.BUY,
+        References=1,
+        Products=[test_product_one, test_product_two],
+        Organisation_id=1,
+    )
+
+
+@pytest.fixture()
+def test_organisation_one() -> OrganisationCreate:
+    return OrganisationCreate(
+        Name="test organisation 1",
+        Type=OrganisationTypeEnum.BUYER,
+    )
+
+
+@pytest.fixture()
+def test_organisation_two() -> OrganisationCreate:
+    return OrganisationCreate(
+        Name="test organisation 2",
+        Type=OrganisationTypeEnum.BUYER,
+    )
+
+
+def test_create_product(test_db: Session, test_product_one: ProductCreate) -> None:
+    product_crud = CRUDProduct(Product)
+    product_creacted = product_crud.create(db=test_db, obj_in=test_product_one)
+    assert product_creacted.id is not None
+    assert product_creacted.Category == test_product_one.Category
+    assert product_creacted.Variety == test_product_one.Variety
+    assert product_creacted.Packaging == test_product_one.Packaging
+
+
+def test_get_product(test_db: Session, test_product_two: ProductCreate) -> None:
+    product_crud = CRUDProduct(Product)
+    product_creacted = product_crud.create(db=test_db, obj_in=test_product_two)
+    product_get = product_crud.get(db=test_db, id=product_creacted.id)
+    assert product_get.id == product_creacted.id
+    assert product_get.Category == test_product_two.Category
+    assert product_get.Variety == test_product_two.Variety
+    assert product_get.Packaging == test_product_two.Packaging
+
+
+def test_get_many_products(
+    test_db: Session, test_product_two: ProductCreate, test_product_three: ProductCreate
+) -> None:
+    product_crud = CRUDProduct(Product)
+    product_creacted2 = product_crud.create(db=test_db, obj_in=test_product_two)
+    product_creacted3 = product_crud.create(db=test_db, obj_in=test_product_three)
+    product_get_many = product_crud.get_multi(db=test_db)
+    assert len(product_get_many) > 1
+    assert product_get_many[-2].id == product_creacted2.id
+    assert product_get_many[-2].Category == test_product_two.Category
+    assert product_get_many[-2].Variety == test_product_two.Variety
+    assert product_get_many[-2].Packaging == test_product_two.Packaging
+    assert product_get_many[-1].id == product_creacted3.id
+    assert product_get_many[-1].Category == test_product_three.Category
+    assert product_get_many[-1].Variety == test_product_three.Variety
+    assert product_get_many[-1].Packaging == test_product_three.Packaging
+
+
+def test_update_product(test_db: Session, test_product_one: ProductCreate) -> None:
+    product_crud = CRUDProduct(Product)
+    product_creacted = product_crud.create(db=test_db, obj_in=test_product_one)
+    product_update = ProductCreate(
+        Category="test category updated",
+        Variety="test variety updated",
+        Packaging="test packaging updated",
+    )
     product_updated = product_crud.update(
         db=test_db, db_obj=product_creacted, obj_in=product_update
     )
-    assert product_updated.id is not None
-    assert product_updated.Category == "test category 2"
-    assert product_updated.Variety == "test variety 2"
-    assert product_updated.Packaging == "test packaging 2"
+    assert product_updated.id == product_creacted.id
+    assert product_updated.Category == product_update.Category
+    assert product_updated.Variety == product_update.Variety
+    assert product_updated.Packaging == product_update.Packaging
 
 
-def test_delete_product(test_db: Session) -> None:
-    product = ProductCreate(
-        Category="test category", Variety="test variety", Packaging="test packaging"
-    )
+def test_delete_product(test_db: Session, test_product_three: ProductCreate) -> None:
     product_crud = CRUDProduct(Product)
-    product_creacted = product_crud.create(db=test_db, obj_in=product)
+    product_creacted = product_crud.create(db=test_db, obj_in=test_product_three)
     product_deleted = product_crud.remove(db=test_db, id=product_creacted.id)
     product_get = product_crud.get(db=test_db, id=product_creacted.id)
-    assert product_deleted.id is not None
-    assert product_deleted.Category == "test category"
-    assert product_deleted.Variety == "test variety"
-    assert product_deleted.Packaging == "test packaging"
+    assert product_deleted.id == product_creacted.id
+    assert product_deleted.Category == test_product_three.Category
+    assert product_deleted.Variety == test_product_three.Variety
+    assert product_deleted.Packaging == test_product_three.Packaging
     assert product_get is None
