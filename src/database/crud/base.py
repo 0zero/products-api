6b68bcade465c9dcee469a5169cb8ea2ed63cb24
Japheth Yates <japheth.yates@gmail.com>
2023-06-18 11:11:31 +0100

    create crud base class

---
+++ src/database/crud/base.py
@@ -0,0 +1,57 @@
+from typing import Any, Dict, List, TypeVar, Union
+
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from src.database.models.base import Base
+
+ModelType = TypeVar("ModelType", bound=Base)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+
+
+class CRUDBase:
+    def __init__(self, model: ModelType):
+        self.model = model
+
+    def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
+        db_obj = self.model(**obj_in.dict())
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def get(self, db: Session, id: int) -> ModelType | None:
+        result = db.query(self.model).filter(self.model.id == id).first()
+        return result if result else None
+
+    def get_multi(
+        self, db: Session, *, skip: int = 0, limit: int = 100
+    ) -> List[ModelType]:
+        result = db.query(self.model).offset(skip).limit(limit).all()
+        return result if result else None
+
+    def update(
+        self,
+        db: Session,
+        db_obj: ModelType,
+        obj_in: Union[UpdateSchemaType, Dict[str, Any]],
+    ) -> ModelType:
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.dict(exclude_unset=True)
+
+        for field, value in update_data.items():
+            setattr(db_obj, field, value)
+
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def remove(self, db: Session, *, id: int) -> ModelType:
+        obj = db.query(self.model).get(id)
+        db.delete(obj)
+        db.commit()
+        return obj
